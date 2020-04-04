69dd7a1505e59ac3d1eac11aa2f4174a5d12405d
thallesdaniel <thallesdaniell@hotmail.com>
2020-04-04 15:24:36 -0300

    craido modelo para tarefas

--- app/Task.php
+++ app/Task.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $hidden =[
+        'updated_at'
+    ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('m/d/Y H:i:s');
+    }
 }
