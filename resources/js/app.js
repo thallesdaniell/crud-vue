ec6f0ad32240b4949924fbf160f398b6be5df41e
thallesdaniel <thallesdaniell@hotmail.com>
2020-04-04 15:37:08 -0300

    adicioando carregamento das dependências e instaciamento

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import {routes} from './routes';
+import App from './App.vue';
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    template: '<App/>',
+    components: { App }
+});
